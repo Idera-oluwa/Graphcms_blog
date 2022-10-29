555675427fa4afa1b2bb117244295eb2be2d6a3c
Isa <ideraoluwaisa2003@gmail.com>
2022-10-29 09:47:34 +0100

    new commit

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -10,7 +10,7 @@ function PostCard({post}) {
                 <img src={post.featuredImage.url} alt={post.title} className='object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg'/>
                 </div>
                 <h1 className='transition duration-700 text-center mb-8 cursor-pointer
-                hover:text-pink-600 text-3xl font-semibold'
+                hover:text-pink-600 text-2xl md:text-3xl font-semibold'
                 >
                 <Link href={`/post/${post.slug}`}>{post.title}</Link>
                 </h1>
