825cea682b234c07f5f8926269edcdde5009fc13
Isa <ideraoluwaisa2003@gmail.com>
2022-10-29 09:53:03 +0100

    initial commit

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -9,7 +9,7 @@ function PostCard({post}) {
                 <div className='relative overflow-hidden shadow-md pb-80 mb-6'>
                 <img src={post.featuredImage.url} alt={post.title} className='object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg'/>
                 </div>
-                <h1 className='transition duration-700 text-center mb-8 cursor-pointer
+                <h1 className='transition duration-700 text-center mb-5 md:mb-8 cursor-pointer
                 hover:text-pink-600 text-2xl md:text-3xl font-semibold'
                 >
                 <Link href={`/post/${post.slug}`}>{post.title}</Link>
@@ -19,7 +19,7 @@ function PostCard({post}) {
                   <img alt={post.authur.name}
                   className='align-middle h-10 w-10 rounded-full' src={post.authur.photo.url}
                   />
-                  <p className='inline align-middle text-gray-700 ml-2 text-lg'>{post.authur.name}</p>
+                  <p className='inline align-middle text-gray-700 ml-2 text-[0.5cm]  md:text-lg'>{post.authur.name}</p>
                 </div>
                 <div className='font-medium text-gray-700'>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -28,7 +28,7 @@ function PostCard({post}) {
         <span className='align-middle'>{moment(post.createdAt).format('MMM DD, YYYY')}</span>
                 </div>
                 </div>
-                <p className='text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8'>{post.excerpt}</p>
+                <p className='text-center md:text-lg text-[0.4cm]  text-gray-700 font-normal px-4 lg:px-20 mb-8'>{post.excerpt}</p>
         <div className='text-center'>
         <Link href={`/post/${post.slug}`}>
         <span className='transition duration-500 transform hover:-translate-y-1 
